1da2e0d72a39377a61ad43e990fd4c6501e8b750
jarra <jarrasplace@gmail.com>
2017-02-16 11:16:00 +1300

    Added Gamefroot_Custom_Files/core/extension_to_google/variables.js and removed colour_variables.js as the content of that file was moved back into variables.js.

--- Gamefroot_Custom_Files/core/extension_to_google/variables.js
+++ Gamefroot_Custom_Files/core/extension_to_google/variables.js
@@ -24,14 +24,189 @@
  */
 'use strict';
 
-goog.provide('Blockly.Variables');
-// goog.require('Blockly.Variables'); // import the core variables file
+// Commented this out as the Blockly.Variables namespace is
+// provided in the core/variables.js file.
+
+// goog.provide('Blockly.Variables');
+
 goog.require('Blockly.Blocks');
 goog.require('Blockly.Workspace');
 goog.require('goog.string');
 
-// This file is the kiwifroot code that has been removed from core/variables.js
-// The name space of the 
+// This file contains the kiwifroot code that has been removed from core/variables.js
+
+/**
+ * The definition name of the any type
+ * @const
+ */
+Blockly.Variables.TYPE_ANY = '';
+/**
+ * The definition name of the boolean type
+ * @const
+ */
+Blockly.Variables.TYPE_BOOLEAN = 'Boolean';
+/**
+ * The definition name of the number type
+ * @const
+ */
+Blockly.Variables.TYPE_NUMBER = 'Number';
+/**
+ * The definition name of the string type
+ * @const
+ */
+Blockly.Variables.TYPE_STRING = 'String';
+/**
+ * The definition name of the colour type
+ * @const
+ */
+Blockly.Variables.TYPE_COLOUR = 'Colour';
+/**
+ * The definition name of the array type
+ * @const
+ */
+Blockly.Variables.TYPE_ARRAY = 'Array';
+/**
+ * The definition name of the array type
+ * @const
+ */
+Blockly.Variables.TYPE_INSTANCE = 'Instance';
+/**
+ * The definition name of the class type
+ * @const
+ */
+Blockly.Variables.TYPE_CLASS = 'Class';
+/**
+ * The definition name of the sound type
+ * @const
+ */
+Blockly.Variables.TYPE_SOUND = 'Sound';
+/**
+ * The definition name of the finger type
+ * @const
+ */
+Blockly.Variables.TYPE_POINTER = 'Pointer';
+/**
+ * The definition name of the location type
+ * @const
+ */
+Blockly.Variables.TYPE_COORDINATE = 'Coordinate';
+
+/**
+ * The colour that should be applied to a block that outputs type 'any'
+ * @const
+ */
+Blockly.Variables.COLOUR_ANY = '#71cd04';
+/**
+ * The colour that should be applied to a block that outputs type 'boolean'
+ * @const
+ */
+Blockly.Variables.COLOUR_BOOLEAN = '#2db1f9';
+/**
+ * The colour that should be applied to a block that outputs type 'number'
+ * @const
+ */
+Blockly.Variables.COLOUR_NUMBER = '#1b6fe9';
+/**
+ * The colour that should be applied to a block that outputs type 'string'
+ * @const
+ */
+Blockly.Variables.COLOUR_STRING = '#40ce9e';
+/**
+ * The colour that should be applied to a block that outputs type 'colour'
+ * @const
+ */
+Blockly.Variables.COLOUR_COLOUR = '#b24ac5';
+/**
+ * The colour that should be applied to a block that outputs type 'array'
+ * @const
+ */
+Blockly.Variables.COLOUR_ARRAY = '#8230e7';
+/**
+ * The colour that should be applied to a block that outputs type 'instance'
+ * @const
+ */
+Blockly.Variables.COLOUR_INSTANCE = '#e74e48';
+/**
+ * The colour that should be applied to a block that outputs type 'class'
+ * @const
+ */
+Blockly.Variables.COLOUR_CLASS = '#fc8607';
+/**
+ * The colour that should be applied to a block that outputs type 'sound'
+ * @const
+ */
+Blockly.Variables.COLOUR_SOUND = '#d147ea';
+/**
+ * The colour that should be applied to a block that outputs type 'finger'
+ * @const
+ */
+Blockly.Variables.COLOUR_POINTER = '#ea8847';
+/**
+ * The colour that should be applied to a block that outs type 'location'
+ * @const
+ */
+Blockly.Variables.COLOUR_COORDINATE = '#388e3c';
+
+/*
+ * New Colour Blocks....
+ * Based on the category a block is in now.
+ */
+Blockly.Variables.COLOUR = {
+    'EVENT': "#edae00",
+    'CONTROL': "#ff8601",
+    'MOTION': "#e54e43",
+    'ANIMATION': "#df358e",
+    'LOOKS': "#b443c9",
+    'SENSING': "#8121e7",
+    'SOUND': "#0f6bf0",
+    'OPERATORS': "#21aefe",
+    'PHYSICS': "#e66b2f",
+    'DRAW': "#38ce9e",
+    'VARIABLES': "#348f32",
+    'FUNCTIONS': "#6bd101",
+    'LOCAL_VARIABLES': "#56ae02",
+    'GLOBAL_VARIABLES': "#006d00"
+};
+
+// This version adds the variables to the default Blockly.Variables namespace for the
+// code in the blocks/extensions_to_google folder as this has not been converted to
+// calling Variables namespace
+
+/*
+ * New Colour Blocks....
+ * Based on the category a block is in now.
+ */
+Blockly.Variables.COLOUR = {
+    'EVENT': "#edae00",
+    'CONTROL': "#ff8601",
+    'MOTION': "#e54e43",
+    'ANIMATION': "#df358e",
+    'LOOKS': "#b443c9",
+    'SENSING': "#8121e7",
+    'SOUND': "#0f6bf0",
+    'OPERATORS': "#21aefe",
+    'PHYSICS': "#e66b2f",
+    'DRAW': "#38ce9e",
+    'VARIABLES': "#348f32",
+    'FUNCTIONS': "#6bd101",
+    'LOCAL_VARIABLES': "#56ae02",
+    'GLOBAL_VARIABLES': "#006d00"
+};
+
+/**
+ * The hue that corresponds to each variable type
+ */
+Blockly.Variables.COLOUR_FOR_TYPE = {};
+Blockly.Variables.COLOUR_FOR_TYPE[Blockly.Variables.TYPE_BOOLEAN] = Blockly.Variables.COLOUR_BOOLEAN;
+Blockly.Variables.COLOUR_FOR_TYPE[Blockly.Variables.TYPE_NUMBER] = Blockly.Variables.COLOUR_NUMBER;
+Blockly.Variables.COLOUR_FOR_TYPE[Blockly.Variables.TYPE_STRING] = Blockly.Variables.COLOUR_STRING;
+Blockly.Variables.COLOUR_FOR_TYPE[Blockly.Variables.TYPE_COLOUR] = Blockly.Variables.COLOUR_COLOUR;
+Blockly.Variables.COLOUR_FOR_TYPE[Blockly.Variables.TYPE_ARRAY] = Blockly.Variables.COLOUR_ARRAY;
+Blockly.Variables.COLOUR_FOR_TYPE[Blockly.Variables.TYPE_INSTANCE] = Blockly.Variables.COLOUR_INSTANCE;
+Blockly.Variables.COLOUR_FOR_TYPE[Blockly.Variables.TYPE_CLASS] = Blockly.Variables.COLOUR_CLASS;
+Blockly.Variables.COLOUR_FOR_TYPE[Blockly.Variables.TYPE_SOUND] = Blockly.Variables.COLOUR_SOUND;
+Blockly.Variables.COLOUR_FOR_TYPE[Blockly.Variables.TYPE_POINTER] = Blockly.Variables.COLOUR_POINTER;
+Blockly.Variables.COLOUR_FOR_TYPE[Blockly.Variables.TYPE_COORDINATE] = Blockly.Variables.COLOUR_COORDINATE;
 
 /**
  * A Complete list of all variables types available.
